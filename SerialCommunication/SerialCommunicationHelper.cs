f6ce5085c1b7f335ab837c779bfd84862ebdc457
RegisterWheat <s224999z@st.me.tuat.ac.jp>
2023-05-09 13:04:02 +0900

    change: readAllをより賢い実装に
    SerialPort.BytesToReadバイト読み込むようにすることで、無駄なバッファリングをなくせる

--- SerialCommunication/SerialCommunicationHelper.cs
+++ SerialCommunication/SerialCommunicationHelper.cs
@@ -5,8 +5,6 @@ namespace SerialCommunication
     class SerialCommunicationHelper
     {
         private SerialPort port;
-        private List<byte> buffer = new();//簡単のためとりあえず富豪的
-        private Mutex bufMutex = new();
         public SerialCommunicationHelper(string portName, int baudRate = 9600, int writeTimeout = 500, int readTimeout = 500)
         {
             port = new()
@@ -16,27 +14,14 @@ namespace SerialCommunication
                 PortName = portName,
                 BaudRate = baudRate
             };
-            port.DataReceived += new SerialDataReceivedEventHandler(dataReceivedHandler);
             port.Open();
             Console.WriteLine($"successfully connected via {port.PortName}");
         }
 
-        private void dataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
-        {
-            bufMutex.WaitOne();
-            for (int i = 0; i < port.BytesToRead; i++)
-            {
-                buffer.Add((byte)port.ReadByte());
-            }
-            bufMutex.ReleaseMutex();
-        }
-
         public byte[] readAll()
         {
-            bufMutex.WaitOne();
-            var result = buffer.ToArray();
-            buffer.Clear();
-            bufMutex.ReleaseMutex();
+            var result = new byte[port.BytesToRead];
+            port.Read(result, 0, port.BytesToRead);
             return result;
         }
     }
